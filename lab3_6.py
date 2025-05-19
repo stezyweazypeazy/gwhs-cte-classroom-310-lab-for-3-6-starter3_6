153d3de1be6627cb512b7aea722f7698cc74cf69
stezyweazypeazy <stevie1202lol@gmail.com>
2025-05-19 13:25:18 -0400

    Update lab3_6.py

--- lab3_6.py
+++ lab3_6.py
@@ -1,7 +1,12 @@
 my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
-#
-# Write your code here.
-#
+temp_list =[]
+
+for nun in my_list:
+    if num not in temp_list:
+        temp_list.append(num)
 print("The list with unique elements only:")
-print(my_list)
+print("temp_list)
+# Write your code here.
+
+
 
